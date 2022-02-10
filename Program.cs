20897ed521245cb73c73de37189454e4f9934a6d
andrewschwabe <schwabea530@gmail.com>
2022-02-09 21:54:09 -0600

    Add swagger comments && update default route

--- Program.cs
+++ Program.cs
@@ -1,12 +1,16 @@
 var builder = WebApplication.CreateBuilder(args);
+
+// Register the required services for Swagger and Swagger UI
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+// Configure Swagger and SwaggerUI
 app.UseSwagger();
 app.UseSwaggerUI();
 
-app.MapGet("/", () => "here");
+// Adding a default route greet the user
+app.MapGet("/", () =>  Results.Ok(new { Message = "Welcome to your phonebook!"}));
 
 app.Run();
